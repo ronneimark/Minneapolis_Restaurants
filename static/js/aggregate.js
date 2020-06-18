f4a17c22db1278377161817a71c29b2788f7d787
Ron's Git <ronneimark@hotmail.com>
2020-06-18 06:59:31 -0500

    Added node packages (ignored), updated Mapbox and switched from external packages

--- static/js/aggregate.js
+++ static/js/aggregate.js
@@ -4,12 +4,12 @@ var map = L.map("map-id", {
 	zoom: 13,
 });
 
-var streets = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-	attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-	zoom: 13,
-	maxZoom: 22,
-	minZoom: 11,
-	id: "mapbox.streets",
+var streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+	tileSize: 512,
+	maxZoom: 18,
+	zoomOffset: -1,
+	id: 'mapbox/streets-v11',
 	accessToken: API_KEY
 }).addTo(map);
 
